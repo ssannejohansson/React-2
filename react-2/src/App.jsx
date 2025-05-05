479c855179ecfa177c24c71853630559df96f711
ssannejohansson <sannej0hansson@hotmail.com>
2025-05-05 17:29:41 +0200

    Finished assignment
    Finished assignment

--- react-2/src/App.jsx
+++ react-2/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import './App.css'
 import API from "./components/API.jsx"
 
--- react-2/src/components/API.jsx
+++ react-2/src/components/API.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import {useEffect, useState} from "react";
+import { useState} from "react";
+import mimmy from "../assets/mimmy.jpg"
+
+
+
+import "../components/API.css"
 
 const API = () => {
 
@@ -15,15 +20,23 @@ const API = () => {
     }
 }; 
 
-
-const generateJoke = () => {
+const generateFact = () => {
     fetchData();
 }
 
 return <div>
-    <button onClick={generateJoke}>Generate fact</button>
-    <p>Fact: {fact.fact}</p>
-</div>;
+    <div className="container">
+        <div className="title" onClick={generateFact}>
+            <img src={mimmy}/>
+            <div className="bubble left">Hi, I'm Mimmy! Click me if you want to know facts about cats</div>
+            </div>
+            <div className="text">
+           <p>{fact.fact}</p>
+        </div>
+    </div>
+    </div>
+   
 }; 
+
 
 export default API;
